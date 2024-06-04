6292e0af48836ab5b879280d9e15fdb02566315e
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-06-04 13:09:15 +0300

    first commit

--- Consumer.py
+++ Consumer.py
@@ -1,5 +1,22 @@
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('kafkaTest')
-for msg in consumer:
-    print(msg)
+bootstrap_servers = ''
+group_id = 'test1'
+topics = ["test111"]
+key = b'111'
+value = b'{"test": 123}'
+
+consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers,
+                         group_id=group_id,
+                         auto_offset_reset="earliest",
+                         enable_auto_commit=True)
+
+consumer.subscribe(topics)
+
+try:
+    for message in consumer:
+        print(message.value.decode('utf-8'))
+except Exception as e:
+    print(f"Error occurred while consuming messages: {e}")
+finally:
+    consumer.close()
