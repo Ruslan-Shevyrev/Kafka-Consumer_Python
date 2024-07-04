63216b7e833409d0e7665918c576bedf2bd8b0c2
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-07-04 10:56:53 +0300

    add consumer commit exception

--- consumer.py
+++ consumer.py
@@ -7,16 +7,23 @@ key = b'111'
 value = b'{"test": 123}'
 
 consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers,
-                         group_id=group_id,
-                         auto_offset_reset="earliest",
-                         enable_auto_commit=True)
+						group_id=group_id,
+						auto_offset_reset="earliest",
+						enable_auto_commit=False)
 
 consumer.subscribe(topics)
 
-try:
-    for message in consumer:
-        print(message.value.decode('utf-8'))
-except Exception as e:
-    print(f"Error occurred while consuming messages: {e}")
-finally:
-    consumer.close()
+for message in consumer:
+	try:
+		consumer.commit()
+	except Exception as e:
+		print('Kafka commit error, continue')
+		time.sleep(1)
+		continue
+	
+	try:
+		print(message.value.decode('utf-8'))
+	except Exception as e:
+		print(f"Error occurred while consuming messages: {e}")
+	finally:
+		consumer.close()
